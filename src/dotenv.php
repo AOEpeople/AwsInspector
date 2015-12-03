4570897a38b55158e6d95dfc8acca0faeba2f43a
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-12-03 03:21:56 +0000

    Removed debug message

--- src/dotenv.php
+++ src/dotenv.php
@@ -1,13 +1,9 @@
 <?php
 
-echo "ERdsdsEsdsR";
-
 if (is_readable(getcwd() . DIRECTORY_SEPARATOR . '.env')) {
-    echo "ERdsdsER";
     $dotenv = new Dotenv\Dotenv(getcwd());
     $dotenv->load();
 } elseif (is_readable(getcwd() . DIRECTORY_SEPARATOR . '.env.default')) {
-    echo "ERER";
     $dotenv = new Dotenv\Dotenv(getcwd(), '.env.default');
     $dotenv->load();
 }
