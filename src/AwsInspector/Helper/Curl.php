aa008d3a9c27885b15b2bc43099742e2f03b21cf
Fabrizio Branca <fabrizio.branca@aoe.com>
2015-12-22 22:38:30 +0000

    Refactored curl helper

--- src/AwsInspector/Helper/Curl.php
+++ src/AwsInspector/Helper/Curl.php
@@ -6,18 +6,56 @@ class Curl
 {
 
     protected $url;
+    protected $headers=[];
+    protected $maxTime = 5;
 
-    public function __construct($url)
+    public function __construct($url, $headers=[])
     {
         $this->url = $url;
+        $this->headers = $headers;
+    }
+
+    protected function getHeaderParams() {
+        $params=[];
+        foreach ($this->headers as $header) {
+            $params[] = "--header '$header'";
+        }
+        return implode(' ', $params);
+    }
+
+    protected function getCurlCommand($params=[]) {
+        $command = [];
+        $command[] = 'curl';
+        $command[] = $this->getHeaderParams();
+        $command[] = '--insecure';
+        $command[] = '--silent';
+        if ($this->maxTime) {
+            $command[] = '--max-time '.$this->maxTime;
+        }
+        $command = array_merge($command, $params);
+        return implode(' ', $command);
     }
 
     public function getResponseStatusCodeCommand() {
-        return 'curl --insecure --silent --output /dev/null --write-out "%{http_code}" ' . escapeshellarg($this->url);
+        return $this->getCurlCommand([
+            '--output /dev/null',
+            '--write-out "%{http_code}"',
+            escapeshellarg($this->url)
+        ]);
+    }
+
+    public function getResponseHeadersCommand() {
+        return $this->getCurlCommand([
+            '--head',
+            escapeshellarg($this->url)
+        ]);
     }
 
     public function getResponseBodyCommand() {
-        return 'curl --insecure --silent ' . escapeshellarg($this->url);
+        return $this->getCurlCommand([
+            escapeshellarg($this->url)
+        ]);
     }
 
 }
+
